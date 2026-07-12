a77ed21a3994f08a4953834f1d90695e18b284b7
ajitshilimkar77-collab <ajitshilimkar77@gmail.com>
2026-07-12 14:57:39 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDFtphUxT-IJCz5v7RZxVTiYKylGa4LZpY",
