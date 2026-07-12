2d844266f51fed2b3711dc5f425601d118024d35
ajitshilimkar77-collab <ajitshilimkar77@gmail.com>
2026-07-12 13:45:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-import { db } from "./firebase.js";
+
+import { auth, db } from "./firebase.js";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 import {
   collection,
@@ -9,29 +16,85 @@ import {
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
+const loginPage = document.getElementById("loginPage");
+const chatPage = document.getElementById("chatPage");
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+const loginBtn = document.getElementById("loginBtn");
+const signupBtn = document.getElementById("signupBtn");
+
 const chat = document.getElementById("chat");
 const msg = document.getElementById("msg");
 const sendBtn = document.getElementById("sendBtn");
 
-sendBtn.addEventListener("click", async () => {
-  if (msg.value.trim() === "") return;
+// Create Account
+signupBtn.onclick = async () => {
+  try {
+    await createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+    alert("Account Created");
+  } catch (e) {
+    alert(e.message);
+  }
+};
+
+// Login
+loginBtn.onclick = async () => {
+  try {
+    await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+  } catch (e) {
+    alert(e.message);
+  }
+};
+
+// Auth State
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    loginPage.style.display = "none";
+    chatPage.style.display = "block";
+  } else {
+    loginPage.style.display = "block";
+    chatPage.style.display = "none";
+  }
+});
+
+// Send Message
+sendBtn.onclick = async () => {
+  if (msg.value.trim() == "") return;
 
   await addDoc(collection(db, "messages"), {
     text: msg.value,
+    sender: auth.currentUser.email,
     time: serverTimestamp()
   });
 
   msg.value = "";
-});
+};
 
+// Show Messages
 const q = query(collection(db, "messages"), orderBy("time"));
 
 onSnapshot(q, (snapshot) => {
   chat.innerHTML = "";
 
   snapshot.forEach((doc) => {
-    const div = document.createElement("div");
-    div.innerText = doc.data().text;
-    chat.appendChild(div);
+    const d = document.createElement("div");
+    d.innerHTML =
+      "<b>" +
+      doc.data().sender +
+      "</b><br>" +
+      doc.data().text +
+      "<hr>";
+
+    chat.appendChild(d);
   });
 });
